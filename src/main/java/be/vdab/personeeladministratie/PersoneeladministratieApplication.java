26c6714cae533384bbccbce47880f384f4657d9a
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-08-28 09:02:32 +0200

    eerste commit

---
+++ src/main/java/be/vdab/personeeladministratie/PersoneeladministratieApplication.java
@@ -0,0 +1,12 @@
+package be.vdab.personeeladministratie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersoneeladministratieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersoneeladministratieApplication.class, args);
+	}
+}
---
+++ src/test/java/be/vdab/personeeladministratie/PersoneeladministratieApplicationTests.java
@@ -0,0 +1,16 @@
+package be.vdab.personeeladministratie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersoneeladministratieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
